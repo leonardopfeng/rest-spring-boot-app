2172bcdde3b384e13e8edc4481d744216c5ec103
Leonardo Pfeng <col.leonardopfeng@uniuv.edu.br>
2024-09-11 18:11:10 -0300

    Allowing CORS to work globally in the application
    Allowed some routes to have access to all implemented methods in the app

--- rest-spring-boot-app/src/main/java/br/com/lelis/config/WebConfig.java
+++ rest-spring-boot-app/src/main/java/br/com/lelis/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.lelis.config;
 
 import br.com.lelis.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,9 +17,22 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        // Allowing all methods, and all configured origins
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
     }
 
     @Override
