3d24b2dc99f04c399a16ab746a0ab73bf683c6b1
Leonardo Pfeng <col.leonardopfeng@uniuv.edu.br>
2024-09-13 10:19:43 -0300

    Adding Spring Security
    Added necessary tables for Spring Security
    Created the connection between 'users' and 'permissions' tables

---
+++ rest-spring-boot-app/src/main/java/br/com/lelis/model/Permission.java
@@ -0,0 +1,61 @@
+package br.com.lelis.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "permission")
+public class Permission implements GrantedAuthority, Serializable {
+
+    private static long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "description", nullable = false, length = 80)
+    private String description;
+
+    public Permission(){}
+
+    public Permission(long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Permission that = (Permission) o;
+        return id == that.id && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
