62abc7a0583cabc3eed2df0f869aaed52cd5549c
Leonardo Pfeng <col.leonardopfeng@uniuv.edu.br>
2025-01-09 18:57:27 -0300

    Adding method to find people by name
    Used query param to search for a specific name given by the user in the database

--- rest-spring-boot-app/src/main/java/br/com/lelis/controllers/PersonController.java
+++ rest-spring-boot-app/src/main/java/br/com/lelis/controllers/PersonController.java
@@ -70,6 +70,47 @@ public class PersonController {
         return ResponseEntity.ok(service.findAll(pageable));
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
+    @GetMapping(
+            value = "/findPersonsByName/{firstName}",
+            produces = {
+            br.com.lelis.util.MediaType.APPLICATION_JSON,
+            br.com.lelis.util.MediaType.APPLICATION_XML,
+            br.com.lelis.util.MediaType.APPLICATION_YML
+    }
+    )
+    @Operation(
+            summary = "Finds a group of Person by name",
+            description = "Finds a group of Person by name",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = PersonVO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            })
+    public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findPersonsByName(
+            @PathVariable(value = "firstName") String firstName,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction
+    ) {
+        var sortDirection = "desc".equalsIgnoreCase(direction)
+                ? Sort.Direction.DESC : Sort.Direction.ASC;
+
+        // Passes to the service the pageable object containing:
+        // The number of the page; How many elements it has; How it'll be ordered
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+        return ResponseEntity.ok(service.findPersonsByName(firstName ,pageable));
+    }
+
     @CrossOrigin(origins = {"http://localhost:8080", "https://lelis.com.br"})
     @GetMapping(value = "/{id}",
             produces = {
